03160d26b165065cc17c243665cd223b826dd7fb
Leo Yu <yuleo@netscape.net>
2015-09-28 20:01:04 -0700

    First commit

--- Euler/main.swift
+++ Euler/main.swift
@@ -8,5 +8,54 @@
 
 import Foundation
 
+func multi35( limit: Int) -> Int{
+    var sum = 0 ;
+    
+    var index = 1;
+    var mySet = Set<Int>();
+    
+    while ( index < limit )
+    {
+        let sum2 = index * 3;
+        if sum2 >= limit
+        {
+            break;
+        }
+        mySet.insert(sum2);
+        //sum += sum2;
+        index++;
+    }
+    
+    index = 1;
+    while ( index < limit )
+    {
+        let sum2 = index * 5;
+        if sum2 >= limit
+        {
+            break;
+        }
+        mySet.insert(sum2)
+        //sum += sum2;
+        index++;
+    }
+    
+    for intNum in mySet
+    {
+        sum += intNum;
+    }
+    return sum;
+}
+
+
+func evenFN( limit : Int ) -> Double {
+    var sum = 0.0;
+    
+    
+    return sum;
+}
+
 print("Hello, World!")
+let sum = multi35(1000);
+
+print("sum is \(sum)");
 
