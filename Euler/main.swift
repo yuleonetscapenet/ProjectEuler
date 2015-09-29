966161cc82441b1bf9c5db93aff6c9378ffd1e22
Leo Yu <yuleo@netscape.net>
2015-09-28 20:56:53 -0700

    even fibonacci number sum

--- Euler/main.swift
+++ Euler/main.swift
@@ -48,14 +48,47 @@ func multi35( limit: Int) -> Int{
 
 
 func evenFN( limit : Int ) -> Double {
-    var sum = 0.0;
+    var sum = 2.0;
+    var sumArray : [Double] = [];
     
+    sumArray.insert(1.0, atIndex: 0);
+    sumArray.insert(2.0, atIndex: 1);
+    
+    //empty set
+    var mySet = Set<Double>();
+    
+    for  var i = 2; i < limit; i++
+    {
+        let t1 = sumArray[i-1] + sumArray[i-2];
+        if  t1 > Double(limit)
+        {
+            //calculate the sum
+            for doubleNum in mySet
+            {
+                sum += doubleNum;
+            }
+            return sum;
+        }
+        
+        if (t1 % 2 == 0 )
+        {
+            mySet.insert(t1);
+        }
+        
+        sumArray.insert(t1, atIndex: i);
+        
+    }
     
     return sum;
 }
 
+//
+//mark - begin main
+//
 print("Hello, World!")
 let sum = multi35(1000);
-
 print("sum is \(sum)");
+
+let evenSum = evenFN(4000000);
+print("evenSum is \(evenSum)");
 
